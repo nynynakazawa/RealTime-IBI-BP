d322352fee6d183074ba2ca1fc9c1b43669622c0
nynynakazawa <nynynakazawa@gmail.com>
2025-08-09 20:31:43 +0900

    chore: initial import

--- train.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-# スリープさせずに学習を続行、Pythonの出力をノンバッファで流す
-caffeinate -i python3 -u scripts/train_tinygpt.py \
-  --data_dir tinygpt/tinystories \
-  --out_dir model_out \
-  --epochs 10 \
-  --batch_size 250000 |& tee train.log
